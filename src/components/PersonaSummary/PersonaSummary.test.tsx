7944f81e2c157b06c949f4816cb5fcbb91f687a3
Jay Smith <jsmithb117@gmail.com>
2023-06-27 16:08:14 -0700

    Completes setup of Jest

--- src/components/PersonaSummary/PersonaSummary.test.tsx
+++ src/components/PersonaSummary/PersonaSummary.test.tsx
@@ -1,8 +1,12 @@
-// import { render } from "@testing-library/react";
-// import PersonaSummary from "./PersonaSummary";
+import { render } from "@testing-library/react";
+import PersonaSummary from "./PersonaSummary";
 
 describe("PersonaSummary", () => {
   it("should work as expected", () => {
+    render(<PersonaSummary persona={{
+      imageURL: "not a url",
+      name: "also not"
+    }} />)
     expect(1 + 1).toBe(2);
   });
 });
