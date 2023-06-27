63fd7a696151f77d20fe9c73a6af8baac499a21a
Jay Smith <jsmithb117@gmail.com>
2023-06-27 15:47:28 -0700

    Creates simple test test

---
+++ src/components/PersonaSummary/PersonaSummary.test.tsx
@@ -0,0 +1,8 @@
+// import { render } from "@testing-library/react";
+// import PersonaSummary from "./PersonaSummary";
+
+describe("PersonaSummary", () => {
+  it("should work as expected", () => {
+    expect(1 + 1).toBe(2);
+  });
+});
