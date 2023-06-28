2f1f6a014d1d1431572ac5bd086788fbc42cecbd
Jay Smith <jsmithb117@gmail.com>
2023-06-28 13:04:31 -0700

    Cleans up components

--- src/components/PersonaSummary/PersonaSummary.tsx
+++ src/components/PersonaSummary/PersonaSummary.tsx
@@ -10,12 +10,7 @@ interface PersonaSummaryProps {
 function PersonaSummary({ persona }: PersonaSummaryProps ){
   return (
     <>
-      {/* <Box sx={{ width: '100%', maxWidth: 500 }}>
-        <Typography variant="h1" gutterBottom>
-            h1. Heading
-        </Typography>
-        </Box> */}
-    <Avatar src={persona.imageURL} alt={`${persona.name} avatar`} />
+      <Avatar src={persona.imageURL} alt={`${persona.name} avatar`} />
     </>
   )
 }
