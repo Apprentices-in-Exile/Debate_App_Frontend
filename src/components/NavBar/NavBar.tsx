d190d673989d2a20a97abd822bc373afabc4aaf1
Cat McClelland <84889884+catmcclelland@users.noreply.github.com>
2023-07-06 16:39:55 -0400

    remove unused import

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -11,7 +11,6 @@ import {
   MenuHandler,
   MenuList,
   MenuItem,
-  Chip
 } from '@material-tailwind/react'
 import PropTypes from 'prop-types'
 import {
@@ -43,29 +42,6 @@ const colors = {
   cyan: 'bg-cyan-50 text-cyan-500',
   pink: 'bg-pink-50 text-pink-500'
 }
-type color =
-  | 'transparent'
-  | 'white'
-  | 'blue-gray'
-  | 'gray'
-  | 'brown'
-  | 'deep-orange'
-  | 'orange'
-  | 'amber'
-  | 'yellow'
-  | 'lime'
-  | 'light-green'
-  | 'green'
-  | 'teal'
-  | 'cyan'
-  | 'light-blue'
-  | 'blue'
-  | 'indigo'
-  | 'deep-purple'
-  | 'purple'
-  | 'pink'
-  | 'red'
-
 
 const navListMenuItems = [
   {
